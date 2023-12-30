70aaae8dc9635bdb8e2948a28c9b41b37eb9d000
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2023-12-30 13:18:12 +0530

    Initial commit

---
+++ test.js
@@ -0,0 +1,5 @@
+import "./artifacts/contracts/registry/SidRegistry.sol/SidRegistry.json";
+import "./artifacts/contracts/admin/SANN.sol/SANN.json";
+import "./artifacts/contracts/controller/RegistrarController.sol/RegistrarController.json";
+import "./artifacts/contracts/proxy/PreRegistrationCreator.sol/PreRegistrationCreator.json";
+// goldsky subgraph deploy mode-domains/0.0.1 --from-abi ./config.json
